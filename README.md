a001a6876680a3350fefaa8da5f7d8c2a3a0fa4a
Alejandro Gomez <alexserverone@gmail.com>
2015-11-05 13:46:18 -0500

    First commit. Adding an API Adding the public files Adding some npm packages

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # learning-backbone
 Demo code to learn about backbone
+
+check the [reference](reference.md)
+
+check the [code](app/)
---
+++ app/api.js
@@ -0,0 +1,63 @@
+/*
+  API
+  It mimics a RESTful API but with fake data.
+
+  TODO: use at least sqlite
+*/
+
+var express = require('express');
+var query = require('simple-object-query');
+var app = express();
+var data = require('./data.json');
+var _ = require('lodash');
+var server;
+
+app.use(express.static('./public'));
+
+app.get('/students', function(req, res) {
+  res.json(data.student);
+});
+
+app.get('/students/:id', function(req, res) {
+  var student = query.where(data.student, { id: parseInt(req.params.id) } );
+  if (student.length > 0) {
+    res.json(student[0]);
+  } else {
+    res.status(500).end();
+  }
+});
+
+app.post('/students', function(req, res) {
+  //no validation since we're learning backbone, not API design, YOLO.
+  var student = req.body;
+  student.id = data.student.length + 1;
+  data.student.push(student);
+  res.status(201).json(student);
+});
+
+app.put('/students/:id', function(req, res) {
+  //no validation since we're learning backbone, not API design, YOLO.
+  var student = query.where(data.student, {id: parseInt(req.params.id)});
+  student = _.extend(student, req.body);
+  res.json({OK: true});
+});
+
+app.get('/subjects', function(req, res) {
+  res.json(data.subject);
+});
+
+app.get('/subjects/:id', function(req, res) {
+  var subject = query.where(data.subject, { id: parseInt(req.params.id) } );
+  if (subject.length > 0) {
+    res.json(subject[0]);
+  } else {
+    res.status(500).end();
+  }
+});
+
+server = app.listen(3000, function() {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
